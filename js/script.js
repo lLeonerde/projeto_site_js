d7cc4fe02792ab3616b75da7522cd631c0eaf688
lLeonerde <leonardo.rborbon@gmail.com>
2024-06-25 21:25:13 -0300

    haha

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('heroImage').addEventListener('click', function() {
+        alert('Você clicou no Super Herói da Informática!');
+    });
+});
+
+function validarFormulario() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+
+    if (isNaN(num1) || isNaN(num2) || num1 === '' || num2 === '') {
+        alert('Por favor, insira números válidos.');
+        return false;
+    }
+
+    var resultado = parseFloat(num1) + parseFloat(num2);
+    document.getElementById('resultado').innerText = 'Resultado: ' + resultado;
+    return false;  // Prevent form submission
+}
